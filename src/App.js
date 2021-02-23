816c174409c07748f2544b9bb5aaef1d003167a9
tjefferson1913 <tnwestbrook29@gmail.com>
2021-02-22 20:00:00 -0500

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <h1>Weather App</h1>
       <footer>
         This project is created by Tiara J and on {" "}
@@ -13,6 +14,7 @@ export default function App() {
         Github
         </a>
       </footer>
+      </div> 
     </div>
   );
 }
