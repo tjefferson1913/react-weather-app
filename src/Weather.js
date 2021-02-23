c40b3520713e688049dc9ca19b50a3a92449873c
tjefferson1913 <tnwestbrook29@gmail.com>
2021-02-22 21:44:12 -0500

    Added styling

--- src/Weather.js
+++ src/Weather.js
@@ -5,31 +5,42 @@ export default function Weather() {
     return(
         <div className="Weather">
             <form>
-                <div className="row">
-                    <div className="col-9">
-                <input 
-                type="search" 
-                placeholder="Please enter a city..."
-                className="form-control" 
+              <div className="row">
+                <div className="col-9">
+                  <input 
+                    type="search" 
+                    placeholder="Please enter a city..."
+                    className="form-control" 
+                    autoFocus="on"
                 />
                 </div>
                 <div className="col-3">
-            <input type="submit" value="Search" className="btn 
-            btn-primary" />
-            </div>
-            </div>
+                  <input 
+                    type="submit"
+                    value="Search" 
+                    className="btn btn-primary w-100" 
+                  />
+                </div>
+              </div>
             </form>
-        <h1> Miami </h1>
-        <ul>
+            <h1> Miami </h1>
+            <ul>
             <li>Monday 20:00</li>
-                <li>Mostly Cloudy</li>
+            <li>Mostly Cloudy</li>
             </ul>
-            <div className ='row'>
+            <div className ="row mt-3">
+            <div className="col-6">
+                <div className="clearfix">
                 <img
                 src='https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png'
                 alt="Mostly Cloudy"
+                className="float-left"
             />
-            80F  
+            <div className="float-left">
+            <span className="temperature">80</span>
+            <span className="unit">°F</span>
+            </div>
+              </div>
             </div>
             <div className="col-6">
                 <ul>
@@ -38,7 +49,7 @@ export default function Weather() {
                     <li>Wind 10km/h</li>
                 </ul>
             </div>
-            </div>
-                
+            </div>  
+            </div> 
     );
 }
