b33d254f5df28d4b25512b73ea749e76f7c88790
tjefferson1913 <tnwestbrook29@gmail.com>
2021-02-24 18:05:49 -0500

    Fixed api

--- src/Weather.js
+++ src/Weather.js
@@ -25,7 +25,7 @@ export default function Weather(props) {
 
       function search() {
         const apiKey = "2936a2aac9698ca0c47a6a60f8ab239e";
-        let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+        let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
         axios.get(apiUrl).then(handleResponse);
       }
 
